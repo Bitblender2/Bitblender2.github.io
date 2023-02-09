9e0869718d7f1a40657449708e4d1374f9736914
AlgoAce <wkaimah2@gmail.com>
2023-02-09 10:31:35 +0200

    my first git repo i created

---
+++ READ me.md
@@ -0,0 +1,2 @@
+this is a git repo that am practising on and soon i will become a git Ace.
+what in the gods mercy is this abale 
---
+++ app.dart
@@ -0,0 +1,6 @@
+void main() {
+  List mpegha = ["mesho", "ine", "deen", "tanga"];
+  for (var mpe in mpegha) {
+    print(mpe);
+  }
+}
